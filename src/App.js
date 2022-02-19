13c23180ea70a7cc0279013d8efa40367b2a366c
WilD <a.nazarenko@nylonfabric.ru>
2022-02-19 19:36:06 +0300

    fix animations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {Home} from '../src/pages/home.js';
+import { Home } from '../src/pages/home.js';
 import { Alert } from "./components/alert.js";
 import { Navbar } from "./components/navbar.js";
-import {About} from './pages/about.js';
+import { About } from './pages/about.js';
 
 function App() {
   return (
--- src/components/notes.js
+++ src/components/notes.js
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchNotes, removeNote, selectors } from "../slices/noteSlice.js";
-import _ from 'lodash';
+import { Empty } from './empty.js';
+
 
 export const Notes = () => {
 
@@ -13,40 +14,26 @@ export const Notes = () => {
         dispatch(fetchNotes());
     }, [dispatch])
 
-    
-    console.log(notes);
-
     const removeHandler = (note) => {
         dispatch(removeNote(note));
     };
 
-    if (notes.length === 0) {
-        return (
-            <div
-                className={'emptynote'}
-                timeout={800}
-                key={_.uniqueId()}>
-                    <div className="container-fluid ">Nothing here...</div>
-            </div>
-        )
-    }
-
     return (
         <TransitionGroup component="ul" className="list-group">
-            {notes.map(({id, note}) => (
+            { notes.map(({id, note}) => (
                 <CSSTransition
-                    className={'note'}
-                    timeout={800}
-                    key={id}>
-                    <li className="list-group-item note" id={id}>
+                    key={note.id}
+                    classNames={'note'}
+                    timeout={500}>
+                    <li className="list-group-item note">
                         <div className="note">
                             <strong>{note.text}</strong>
                             <small>{new Date().toLocaleDateString()}</small>
                         </div>
                         <button onClick={() => removeHandler(id)} type="button" className="btn btn-outline-danger btn-sm"><i className="bi bi-trash-fill"></i></button>
                     </li>
-                </CSSTransition>
-            ))}
+                </CSSTransition>))
+            }
             </TransitionGroup>
     );
 }
