54ee2e6571347790231b0a2a8037d349258deb99
WilD <a.nazarenko@nylonfabric.ru>
2022-02-20 18:10:00 +0300

    small beauty

--- src/components/form.js
+++ src/components/form.js
@@ -1,39 +1,37 @@
 import React, { useState } from 'react';
-import { useDispatch } from "react-redux";
+import { useDispatch } from 'react-redux';
 import { show, hide } from '../slices/alertSlice';
 import { sendNote } from '../slices/noteSlice';
-
 import _ from 'lodash';
 
 export const Form = () => {
+  const [value, setValue] = useState('');
 
-    const [value, setValue] = useState('');
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-
-    const submitHandler = (e) => {
-        e.preventDefault();
-        if (value.trim()){
-            dispatch(show({ visible: true, text: 'Note succesfull added', type: 'primary' }));
-            dispatch(sendNote({ id: _.uniqueId(), text: value }))
-            setValue('');
-            setTimeout(() => dispatch(hide()), 1500);
-        } else {
-            dispatch(show({ visible: true, text: 'Please write note before save it' }));
-        }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (value.trim()) {
+      dispatch(show({ visible: true, text: 'Note succesfull added', type: 'primary' }));
+      dispatch(sendNote({ id: _.uniqueId(), text: value }));
+      setValue('');
+      setTimeout(() => dispatch(hide()), 1500);
+    } else {
+      dispatch(show({ visible: true, text: 'Please write note before save it' }));
     }
+  };
 
-    return (
-        <form onSubmit={submitHandler}>
-            <div className='form-group'>
-                <input
-                    type='text'
-                    className='form-control'
-                    placeholder='Please enter here a new note for record'
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                />
-            </div>
-        </form>
-    )
-}
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="form-group">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Please enter here a new note for record"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+    </form>
+  );
+};
--- src/components/notes.js
+++ src/components/notes.js
@@ -1,47 +1,46 @@
-import React, { useEffect } from "react";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchNotes, removeNote, selectors} from "../slices/noteSlice.js";
+import React, { useEffect } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchNotes, removeNote, selectors } from '../slices/noteSlice.js';
 import { Empty } from './empty.js';
 import { show, hide } from '../slices/alertSlice';
 
-
 export const Notes = () => {
+  const dispatch = useDispatch();
+  const notes = useSelector(selectors.selectAll);
 
-    const dispatch = useDispatch();
-    const notes = useSelector(selectors.selectAll);
-    const status = useSelector((state) => state.notes.loading);
+  useEffect(() => {
+    dispatch(fetchNotes());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(fetchNotes());
-    }, [dispatch])
+  const removeHandler = (note) => {
+    dispatch(show({ visible: true, text: 'Processing delete...', type: 'warning' }));
+    dispatch(removeNote(note));
+    setTimeout(() => dispatch(hide()), 1500);
+  };
 
-    const removeHandler = (note) => {
-        dispatch(show({ visible: true, text: 'Processing delete...', type: 'warning' }))
-        dispatch(removeNote(note));
-        setTimeout(() => dispatch(hide()), 1500);
-    };
+  if (notes.length === 0) {
+    return <Empty />;
+  }
 
-    if (notes.length === 0) {
-        return <Empty />
-    }
-
-    return (
-        <TransitionGroup component="ul" className="list-group">
-            { notes.map(({id, note}) => (
-                <CSSTransition
-                    key={note.id}
-                    classNames={'note'}
-                    timeout={500}>
-                    <li className="list-group-item note">
-                        <div className="note">
-                            <strong>{note.text}</strong>
-                            <small>{new Date().toLocaleDateString()}</small>
-                        </div>
-                        <button onClick={() => removeHandler(id)} type="button" className="btn btn-outline-danger btn-sm"><i className="bi bi-trash-fill"></i></button>
-                    </li>
-                </CSSTransition>))
-            }
-            </TransitionGroup>
-    );
-}
+  return (
+    <TransitionGroup component="ul" className="list-group">
+      {notes.map(({ id, note }) => (
+        <CSSTransition key={note.id} classNames={'note'} timeout={500}>
+          <li className="list-group-item note">
+            <div className="note">
+              <strong>{note.text}</strong>
+              <small>{new Date().toLocaleDateString()}</small>
+            </div>
+            <button
+              onClick={() => removeHandler(id)}
+              type="button"
+              className="btn btn-outline-danger btn-sm">
+              <i className="bi bi-trash-fill"></i>
+            </button>
+          </li>
+        </CSSTransition>
+      ))}
+    </TransitionGroup>
+  );
+};
